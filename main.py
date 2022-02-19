e65c6c2ecf8177ddd3a0dbd3ce5af18dc8c22b51
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2022-02-19 00:08:37 -0500

    added okay and cancel button to color picker

--- main.py
+++ main.py
@@ -187,12 +187,12 @@ def main():
             [sg.Text("Click a color to set it as the editor's color")],
             [sg.Listbox(values=color_list,
                       size=(20, 12), key='-LIST-', enable_events=True)],
-            [sg.Button('Exit')]]
+            [sg.Button('Okay'), sg.Button('Cancel')]]
 
             color_window = sg.Window('Color Browser', layout) 
             while True:  # Event Loop
                 event, values = color_window.read()
-                if event == 'Exit':
+                if event == 'Okay':
                     if len(values['-LIST-']) != 0:
                         sg.theme(values['-LIST-'][0])
                         config.write_color(values['-LIST-'][0])
@@ -200,7 +200,8 @@ def main():
                         color_window.close()
                         window = reloadwindow(window, sections, column_key)
                         break
-                elif event == None:
+                elif event is None or event == "Cancel":
+                    color_window.close()
                     break
 
         elif event == sg.WIN_CLOSED:
