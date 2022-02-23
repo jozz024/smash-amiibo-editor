b2cee5e688103856b9565a08f522c390df2494f6
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-02-22 21:33:50 -0600

    docstrings :+1:

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ import json
 
 class Config:
     def __init__(self):
+        """Initializes the config class.
+        """
         # opens the config if it exists
         if os.path.exists(os.path.join('resources', 'config.json')):
             with open(os.path.join('resources', 'config.json')) as config:
                 self.config = json.load(config)
         else:
-            # if config doesnt exist, create it and write brackets so it can be loaded by the json module
+            # if config doesn't exist, create it and write brackets so it can be loaded by the json module
             open(os.path.join('resources', 'config.json'), 'w+').write('{}')
             with open(os.path.join('resources', 'config.json')) as config:
                 self.config = json.load(config)
@@ -20,7 +22,7 @@ class Config:
                 self.write_key_path(
                     (os.path.join('resources', 'unfixed-info.bin'), os.path.join('resources', 'locked-secret.bin')))
             elif os.path.exists(os.path.join('resources', 'key_retail.bin')):
-                self.write_key_path([os.path.join('resources', 'key_retail.bin')])
+                self.write_key_path((os.path.join('resources', 'key_retail.bin'), ))
 
         if self.get_region_path() is None:
             if os.path.exists(os.path.join('resources', 'regions.json')):
@@ -30,11 +32,16 @@ class Config:
 
         if self.get_update_status() is None:
             self.set_update(True)
-        
+
         if self.get_color() is None:
             self.write_color('DarkBlue3')
 
     def write_key_path(self, key_path: tuple):
+        """Writes the key path(s) to the config file.
+
+        Args:
+            key_path (tuple): One or more key paths for the config to write.
+        """
         # if there is more than one key path, parse into unfixed info and locked secret and remove key retail from config
         if len(key_path) != 1:
             for keys in key_path:
@@ -55,6 +62,11 @@ class Config:
                 del self.config['locked-secret']
 
     def read_keys(self):
+        """Reads the key file path(s) from config.
+
+        Returns:
+            list: List of key files if found.
+        """
         # if keys in config, return that path
         if 'keys' in self.config:
             return self.config['keys']
@@ -66,10 +78,20 @@ class Config:
             return None
 
     def write_region_path(self, region_path):
+        """Writes the region path given to config.
+
+        Args:
+            region_path (str): Path to regions file.
+        """
         # writes the given region path to regions
         self.config['regions'] = region_path
 
     def get_region_type(self):
+        """Gets the region type from config.
+
+        Returns:
+            str: Region filetype.
+        """
         # check if regions exist
         if 'regions' in self.config:
             # returns the path extension
@@ -79,6 +101,11 @@ class Config:
             return None
 
     def get_region_path(self):
+        """Gets the region path from config.
+
+        Returns:
+            str: Region path.
+        """
         # check if regions exist
         if 'regions' in self.config:
             # return region path
@@ -88,25 +115,48 @@ class Config:
             return None
 
     def save_config(self):
+        """Saves the configuration file.
+        """
         # saves the config file
         with open(os.path.join('resources', 'config.json'), 'w+') as cfg:
             json.dump(self.config, cfg, indent=4)
 
     def set_update(self, truefalse: bool):
+        """Sets the update prompt config.
+
+        Args:
+            truefalse (bool): Whether to set it to true or false.
+        """
         # set update status to given bool
         self.config['prompt_update'] = truefalse
 
     def get_update_status(self):
+        """_summary_
+
+        Returns:
+            bool: If the program should show the update prompt.
+        """
         # check if updates are allowed
         if 'prompt_update' in self.config:
             return self.config['prompt_update']
         else:
             return None
-    
+
     def write_color(self, color):
+        """Writes the given color to config.
+
+        Args:
+            color (str): PSG Theme str.
+        """
+        # writes the theme given.
         self.config['theme'] = color
 
     def get_color(self):
+        """Gets the color from config.
+
+        Returns:
+            str: PSG Theme Str
+        """
         if 'theme' in self.config:
             return self.config['theme']
         else:
--- updater.py
+++ updater.py
@@ -6,6 +6,12 @@ from config import Config
 
 class Updater():
     def __init__(self, ver_num, config: Config):
+        """Initializes the updater class.
+
+        Args:
+            ver_num (str): The version number of the program.
+            config (Config): The config object.
+        """
         # the github class, used to get the data we need
         self.git = Github()
 
@@ -13,6 +19,11 @@ class Updater():
 
         self.version_number = ver_num
     def check_for_update(self):
+        """Checks for an update in the program.
+
+        Returns:
+            bool: Whether or not there is an update available.
+        """
         try:
             # grabs the latest release and assets
             release = self.get_release()
@@ -20,12 +31,12 @@ class Updater():
 
             do_update = False
             check_update = False
-            #checks for version difference, it can probably be done better but i havent figured out a way yet
-            if release.tag_name[1:].split('.')[0] > self.version_number.split('.')[0]: 
+            # checks for version difference, it can probably be done better but i haven't figured out a way yet
+            if release.tag_name[1:].split('.')[0] > self.version_number.split('.')[0]:
                 check_update = True
-            elif release.tag_name[1:].split('.')[1] > self.version_number.split('.')[1]: 
+            elif release.tag_name[1:].split('.')[1] > self.version_number.split('.')[1]:
                 check_update = True
-            elif release.tag_name[1:].split('.')[2] > self.version_number.split('.')[2]: 
+            elif release.tag_name[1:].split('.')[2] > self.version_number.split('.')[2]:
                 check_update = True
             else:
                 return False
@@ -33,7 +44,7 @@ class Updater():
             if self.config.get_update_status() == False:
                 check_update = False
                 return True
-            #check for update if there was a version difference and config 
+            #check for update if there was a version difference and config
             if check_update == True:
                 do_update = self.show_update_prompt()
 
@@ -45,25 +56,48 @@ class Updater():
             return True
         except:
             return False
-        
+
 
     def update(self, assets):
-            # if update is true, get the file data and write it to the zip file
-            r = requests.get(assets.browser_download_url)
-            open(f'temp.{assets.name.split(".")[1]}', 'wb').write(r.content)
-            # run the updater exe and exit 
-            os.startfile(os.path.join('resources', 'update.exe'))
-            os._exit(0)
+        """Updates the program.
+
+        Args:
+            assets (assets): The assets object to download from
+        """
+        # if update is true, get the file data and write it to the zip file
+        r = requests.get(assets.browser_download_url)
+        open(f'temp.{assets.name.split(".")[1]}', 'wb').write(r.content)
+        # run the updater exe and exit
+        os.startfile(os.path.join('resources', 'update.exe'))
+        os._exit(0)
 
     def get_release(self):
+        """Gets the latest release of the program.
+
+        Returns:
+            release: The latest release of the program.
+        """
         release = self.git.get_repo('jozz024/smash-amiibo-editor').get_latest_release()
         return release
-        
+
     def get_assets(self, release):
+        """Gets the assets from the given release.
+
+        Args:
+            release: The release to get the assets from.
+
+        Returns:
+            assets: The assets object.
+        """
         assets = release.get_assets()[0]
         return assets
-        
+
     def show_update_prompt(self):
+        """Shows the update prompt of the program.
+
+        Returns:
+            bool: If they said yes or no to the update.
+        """
         # sets the window up
         window = sg.Window('Update', [[sg.Text('Would you like to update the program?')], [sg.Button('yes', key= 'YES', enable_events=True), sg.Button('no', key = "NO", enable_events=True)]])
         window.finalize()
