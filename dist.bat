0889ce162e5af568bd38bd697c1b4416dfb75c62
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-03-20 09:08:10 -0500

    make dist script

---
+++ dist.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+pyinstaller main.spec
+
+mv dist/SmashAmiiboEditor.exe dist/SmashAmiiboEditor/SmashAmiiboEditor.exe
+
+cp resources/regions.json dist/SmashAmiiboEditor/resources/regions.json
+
+cd dist
+
+cd SmashAmiiboEditor
+
+zip SmashAmiiboEditor.zip SmashAmiiboEditor.exe resources/regions.json templates/*
