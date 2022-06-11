cdf8f5830415bb36476d0d504cd150504ada21cb
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-06-11 11:54:09 -0500

    version bump

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def main():
         os.remove(os.path.join(os.getcwd(), "update.exe"))
 
     column_key = "COLUMN"
-    version_number = "1.3.1"
+    version_number = "1.4.0"
     # initializes the config class
     config = Config()
     update = Updater(version_number, config)
