bdf2079e6f583baf4cee817cd1862dc887911d4c
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2023-06-17 14:41:09 -0400

    updated contact info + costume info

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def main():
         os.remove(os.path.join(os.getcwd(), "update.exe"))
 
     column_key = "COLUMN"
-    version_number = "1.5.1"
+    version_number = "1.5.2"
     # initializes the config class
     config = Config()
     update = Updater(version_number, config)
