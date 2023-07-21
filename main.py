649426622d457c5813ffd85927f90b2873705c8e
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2023-07-21 19:50:05 -0400

    added pythra

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def main():
         os.remove(os.path.join(os.getcwd(), "update.exe"))
 
     column_key = "COLUMN"
-    version_number = "1.5.2"
+    version_number = "1.5.3"
     # initializes the config class
     config = Config()
     update = Updater(version_number, config)
